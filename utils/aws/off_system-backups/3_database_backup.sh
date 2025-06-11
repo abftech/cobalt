9b9e8c186f3accfa7539285091ca5cec1bc13b0a
Mark Guthrie <m@rkguthrie.com>
2025-06-11 15:36:58 +1000

    off system backups

--- utils/aws/off_system-backups/3_database_backup.sh
+++ utils/aws/off_system-backups/3_database_backup.sh
@@ -10,6 +10,10 @@
 # Dump file
 echo "Running pg_dump to extract the data..."
 
+# aws rds restore-db-instance-from-db-snapshot --db-instance-identifier testcli --db-snapshot-identifier rds:cobalt-test-pg17-2025-06-10-16-51 --db-instance-class "db.t3.micro"
+
+# aws rds describe-db-instances --db-instance-identifier testcli
+
 # Run pg_dump and check return code
 if ! pg_dump \
              --exclude-table-data "public.notifications_abstractemail" \
