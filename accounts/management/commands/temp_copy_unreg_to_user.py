52cbd8784302f2085f1ff8bccd5b2ffdf7a07668
Mark Guthrie <m@rkguthrie.com>
2025-11-12 12:55:02 +1100

    script to convert unreg to users

---
+++ accounts/management/commands/temp_copy_unreg_to_user.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from accounts.models import User, UnregisteredUser
+
+""" temporary script to move unregistered users across to become Users """
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+        for player in UnregisteredUser.all_objects.all()[:2]:
+            new_user = User()
+            new_user.email = "fixlater@broken.com"
+            new_user.system_number = player.system_number
+            if player.internal_system_number:
+                new_user.user_type = User.UserType.CONTACT
+            else:
+                new_user.user_type = User.UserType.UNREGISTERED
+            new_user.first_name = player.first_name
+            new_user.last_name = player.last_name
+            new_user.username = player.system_number
+            new_user.save()
+            player.delete()
+            print(f"Create new user {new_user}. Deleted old unreg user {player}")
