7a35a62262defc32de76ae6feac5443a5e8240e1
Mark Guthrie <m@rkguthrie.com>
2025-11-29 12:41:29 +1100

    additional tabs for Excel download for club finance

--- payments/views/org_report/xls.py
+++ payments/views/org_report/xls.py
@@ -1,13 +1,21 @@
+from time import strptime
+
 import xlsxwriter
+from django.db.models import Q
 from django.http import HttpResponse
 
+from club_sessions.models import Session
 from cobalt.settings import GLOBAL_CURRENCY_SYMBOL
+from events.models import Event
+from payments.models import OrganisationTransaction
 from payments.views.org_report.data import (
     organisation_transactions_by_date_range,
     event_payments_summary_by_date_range,
     sessions_and_payments_by_date_range,
     combined_view_events_sessions_other,
+    club_membership_summary_by_date_range,
 )
+from payments.views.org_report.utils import start_end_date_to_datetime
 from utils.views.xls import XLSXStyles
 
 
@@ -187,6 +195,159 @@ def _organisation_transactions_xls_download_combined(
             msg = f"Payments of {GLOBAL_CURRENCY_SYMBOL}{org_tran['amount_outside_range']} were made outside the date range"
             details_sheet.write(row_no, 11, msg, formats.warning_message)
             details_sheet.set_column("L:L", 100)
+
+
+def _organisation_transactions_xls_download_membership(
+    formats, details_sheet, request, club, start_date, end_date
+):
+    """sub of organisation_transactions_xls_download to handle the membership tab"""
+
+    _organisation_transactions_xls_header(
+        request,
+        club,
+        details_sheet,
+        formats,
+        title=f"Download for {start_date} to {end_date}",
+        subtitle="Memberships",
+        subtitle_style=formats.h1_success,
+        width=10,
+    )
+
+    # Now do data headings
+    _details_headings(details_sheet, formats, show_balance=False)
+
+    # Get data
+    start_datetime, end_datetime = start_end_date_to_datetime(start_date, end_date)
+
+    membership_transactions = OrganisationTransaction.objects.filter(
+        organisation=club,
+        created_date__gte=start_datetime,
+        created_date__lte=end_datetime,
+        type="Club Membership",
+    )
+
+    # Data rows
+    for row_no, org_tran in enumerate(membership_transactions, start=12):
+        details_sheet.write(
+            row_no,
+            0,
+            org_tran.created_date.strftime("%Y-%m-%d %H:%M:%S"),
+            formats.detail_row_data,
+        )
+        details_sheet.write(
+            row_no, 1, org_tran.member.full_name, formats.detail_row_data
+        )
+        details_sheet.write(row_no, 2, org_tran.reference_no, formats.detail_row_data)
+        details_sheet.write(row_no, 3, org_tran.id, formats.detail_row_number)
+        details_sheet.write(row_no, 4, org_tran.type, formats.detail_row_data)
+        details_sheet.write(row_no, 5, org_tran.description, formats.detail_row_data)
+        details_sheet.write(row_no, 6, "", formats.detail_row_data)
+        details_sheet.write(row_no, 7, "", formats.detail_row_data)
+        details_sheet.write(row_no, 8, "", formats.detail_row_data)
+        details_sheet.write(row_no, 9, "", formats.detail_row_data)
+        details_sheet.write(row_no, 10, org_tran.amount, formats.detail_row_money)
+
+
+def _organisation_transactions_xls_download_other(
+    formats, details_sheet, request, club, start_date, end_date
+):
+    """sub of organisation_transactions_xls_download to handle the other tab"""
+
+    _organisation_transactions_xls_header(
+        request,
+        club,
+        details_sheet,
+        formats,
+        title=f"Download for {start_date} to {end_date}",
+        subtitle="Other",
+        subtitle_style=formats.h1_success,
+        width=10,
+    )
+
+    # Now do data headings
+    _details_headings(details_sheet, formats, show_balance=False)
+
+    # Get data
+    start_datetime, end_datetime = start_end_date_to_datetime(start_date, end_date)
+
+    other_transactions = (
+        OrganisationTransaction.objects.filter(
+            organisation=club,
+            created_date__gte=start_datetime,
+            created_date__lte=end_datetime,
+        )
+        .exclude(
+            type__in=[
+                "Settlement",
+                "Club Payment",
+                "Club Membership",
+            ]
+        )
+        .exclude(club_session_id__isnull=False)
+        .exclude(
+            Q(type__in=["Entry to an event", "Refund"]) & Q(event_id__isnull=False)
+        )
+    )
+
+    # Get session names
+    session_ids = []
+    for item in other_transactions:
+        if item.club_session_id and item.club_session_id not in session_ids:
+            session_ids.append(item.club_session_id)
+
+    sessions = Session.objects.filter(id__in=session_ids)
+
+    sessions_dict = {session.id: session.description for session in sessions}
+
+    # Get event names
+    event_ids = []
+    for item in other_transactions:
+        if item.event_id and item.event_id not in event_ids:
+            event_ids.append(item.event_id)
+
+    events = Event.objects.filter(id__in=event_ids).select_related("congress")
+    events_dict = {
+        event.id: f"{event.congress} - {event.event_name}" for event in events
+    }
+
+    # Data rows
+    for row_no, other_tran in enumerate(other_transactions, start=12):
+        details_sheet.write(
+            row_no,
+            0,
+            other_tran.created_date.strftime("%Y-%m-%d %H:%M:%S"),
+            formats.detail_row_data,
+        )
+        details_sheet.write(
+            row_no, 1, other_tran.member.full_name, formats.detail_row_data
+        )
+        details_sheet.write(row_no, 2, other_tran.reference_no, formats.detail_row_data)
+        details_sheet.write(row_no, 3, other_tran.id, formats.detail_row_number)
+        details_sheet.write(row_no, 4, other_tran.type, formats.detail_row_data)
+        details_sheet.write(row_no, 5, other_tran.description, formats.detail_row_data)
+        details_sheet.write(
+            row_no, 6, other_tran.club_session_id, formats.detail_row_data
+        )
+
+        if other_tran.club_session_id:
+            session_name = sessions_dict.get(
+                other_tran.club_session_id, "Unknown session - may be deleted"
+            )
+        else:
+            session_name = ""
+
+        details_sheet.write(row_no, 7, session_name, formats.detail_row_data)
+        details_sheet.write(row_no, 8, other_tran.event_id, formats.detail_row_data)
+
+        if other_tran.event_id:
+            event_name = events_dict.get(
+                other_tran.event_id, "Unknown event - may be deleted"
+            )
+        else:
+            event_name = ""
+
+        details_sheet.write(row_no, 9, event_name, formats.detail_row_data)
+        details_sheet.write(row_no, 10, other_tran.amount, formats.detail_row_money)
 
 
 def _organisation_transactions_xls_download_sessions(
@@ -402,6 +563,8 @@ def organisation_transactions_xls_download(
     sessions_sheet = workbook.add_worksheet("Sessions")
     events_sheet = workbook.add_worksheet("Events")
     combined_sheet = workbook.add_worksheet("Combined")
+    membership_sheet = workbook.add_worksheet("Membership")
+    other_sheet = workbook.add_worksheet("Other")
 
     # Create styles
     formats = XLSXStyles(workbook)
@@ -429,6 +592,16 @@ def organisation_transactions_xls_download(
     # Combination tab
     _organisation_transactions_xls_download_combined(
         formats, combined_sheet, request, club, start_date, end_date
+    )
+
+    # Membership tab
+    _organisation_transactions_xls_download_membership(
+        formats, membership_sheet, request, club, start_date, end_date
+    )
+
+    # Other tab
+    _organisation_transactions_xls_download_other(
+        formats, other_sheet, request, club, start_date, end_date
     )
 
     workbook.close()
