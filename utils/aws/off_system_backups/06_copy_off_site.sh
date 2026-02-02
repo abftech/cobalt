03ca93bc42fe8e1c9afe800fc505f9efd9f9ba3f
Mark Guthrie <m@rkguthrie.com>
2026-02-03 09:40:58 +1100

    offsite backups

--- utils/aws/off_system_backups/06_copy_off_site.sh
+++ utils/aws/off_system_backups/06_copy_off_site.sh
@@ -8,13 +8,13 @@
 LATEST_DB=$(ls -rt "$DUMP_FILE"* | tail -n 1)
 LATEST_FILE_SYSTEM=$(ls -rt "$FILE_SYSTEM_DIRECTORY"/*.tar.gz | tail -n 1)
 
-if ! aws s3 --profile backblaze cp $"$LATEST_DB" s3://abf-offsite-backups
+if ! aws s3 --profile backblaze cp "$LATEST_DB" s3://abf-offsite-backups
 then
   ./notify.sh error "Error copying DB file to BackBlaze"
   exit 1
 fi
 
-if ! aws s3 --profile backblaze cp $"$LATEST_FILE_SYSTEM" s3://abf-offsite-backups
+if ! aws s3 --profile backblaze cp "$LATEST_FILE_SYSTEM" s3://abf-offsite-backups
 then
   ./notify.sh error "Error copying DB file to BackBlaze"
   exit 1
